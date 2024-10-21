d63a38f4d973f44e85e7f24f27a28fb1fc6c8cca
Homer A. Petralba <166639412+homerpet8@users.noreply.github.com>
2024-10-21 16:39:57 +0800

    Delete combine_data.sql

--- combine_data.sql
+++
@@ -1,9 +0,0 @@
---Combining the monthly tables to one quarter table
-
-CREATE TABLE fluted-expanse-414509.Cyclistic.divvy_trips_2023_q1 AS (
-  SELECT * FROM `fluted-expanse-414509.Cyclistic.202301-divvy-tripdata`
-  UNION ALL
-  SELECT * FROM `fluted-expanse-414509.Cyclistic.202302-divvy-tripdata`
-  UNION ALL
-  SELECT * FROM `fluted-expanse-414509.Cyclistic.202303-divvy-tripdata`
-  );
